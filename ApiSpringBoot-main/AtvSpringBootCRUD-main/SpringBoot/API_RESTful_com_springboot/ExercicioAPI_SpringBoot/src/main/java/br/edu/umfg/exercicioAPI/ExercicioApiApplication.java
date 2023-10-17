d9d40702079491de82826c8fe3fce19ee27bea2c
duzanzin <115120820+duzanzin@users.noreply.github.com>
2023-10-16 21:56:57 -0300

    Add files via upload

---
+++ ApiSpringBoot-main/AtvSpringBootCRUD-main/SpringBoot/API_RESTful_com_springboot/ExercicioAPI_SpringBoot/src/main/java/br/edu/umfg/exercicioAPI/ExercicioApiApplication.java
@@ -0,0 +1,14 @@
+package br.edu.umfg.exercicioAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author Zanzin
+ */
+@SpringBootApplication
+public class ExercicioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioApiApplication.class, args);
+	}
+}
